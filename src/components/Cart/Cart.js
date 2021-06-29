53fb483288c7dfaafaea19f9ef04054b315affb0
Safei-Ashraf <safei.ashraf@gmail.com>
2021-06-29 10:30:41 +0200

    adding modal layout

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,20 +1,21 @@
+import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
 const Cart = (props) => {
   const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 14.99 }].map(
     (item) => <li>{item.name}</li>
   );
   return (
-    <div>
+    <Modal>
       <ul className={classes[`cart-items`]}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>44.4</span>
       </div>
-      <div className={classes.action}>
+      <div className={classes.actions}>
         <button className={classes.button}>Close X</button>
         <button className={classes.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
